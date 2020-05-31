c08d4420961b8201f8ba80170b5df8c1bf544345
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-06-01 00:55:55 +0530

    PreComputePrime

--- PreComputePrime.java
+++ PreComputePrime.java
@@ -1,42 +1,41 @@
+//https://www.hackerearth.com/practice/math/number-theory/basic-number-theory-2/practice-problems/algorithm/sum-of-primes-7/description/?layout=old
 import java.util.*;
-public class PreComputePrime{
-	public static void main(String[] ars){
-		Scanner sc=new Scanner(System.in);
-		int a[]=new int [10001];
-		Arrays.fill(a,0);
-		// for (int j=0;j<1000000;j++){
-			// System.out.println(a[j]);
-		// }
-		primeSteive(a);
-		int n=sc.nextInt();
-		for (int i=3;i<=n;i+=2){
-			System.out.println(i);
-		}
-		int csum[]=new int [10001];
-		Arrays.fill(csum,0);
-		for(int i=1;i<=10000;i++){
-			csum[i]=csum[i-1]+a[i];
-		}
-		System.out.println("Enter the test cases");
-		int t=sc.nextInt();
-		while(t-->0){
-			System.out.println("Enter the x and y from between you find prime number");
-			int x=sc.nextInt();
-			int y=sc.nextInt();
-			System.out.println(csum[y]-csum[x-1]);
-		}
-	}
-	public static void primeSteive(int a[]){
-		for (int i=3;i<=10000;i+=2){
-			a[i]=1;
-		}
-		for (int i=3;i<=10000;i+=2){
-			if (a[i]==1){
-				for(int j=i*i;j<=10000;j=i+j){
-					a[j]=0;
-				}
-			}
-		}
-		a[2]=1;
-	}
+public class PreComputePrimeja{
+    public static void main(String[] args){
+	//range of prime number
+        final int limit=1000001;
+	//prime number
+        long prime[]=new long[limit];
+        for(int i=2;i<limit;i++){
+            if (i%2!=0)
+                prime[i]=i;
+        }
+        for(int i=2;i*i<limit;i++){
+            if (prime[i]!=0){
+                for (int j=i*i;j<limit;j+=i)
+                    prime[j]=0;
+            }
+        }
+        prime[2]=2;
+        long sum[]=new long[(int)limit];
+	//Sum of prime number upto this range
+        for(int i=1;i<limit-1;i++)
+        {
+            sum[i]=sum[i-1]+prime[i];
+        }
+	Scanner sc=new Scanner(System.in);
+	//Test Cases
+        int t=sc.nextInt();
+	//it is reduce the much time
+        StringBuffer sb=new StringBuffer();
+        while(t-->0){
+	    //Starting range
+            int l=sc.nextInt();
+	    //upto this range
+            int r=sc.nextInt();
+            sb.append(sum[r]-sum[l-1]+"\n");
+        }
+	//we will print all test cases
+         System.out.println(sb.toString());
+    }
 }
